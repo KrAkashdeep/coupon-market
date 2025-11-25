e42d85a912262a3d7a9cbc0584f5b192005ad01b
KrAkashdeep <akashdeep93140@gmail.com>
2025-11-25 20:12:47 +0530

    edited

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,6 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
-      "http://localhost:5173",
-      "https://mycouponmarket.vercel.app"
     ],
     credentials: true,
   })
